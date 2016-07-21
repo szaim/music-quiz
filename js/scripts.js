bd780d0e0a200dbe1c1e41c4ac1790919f7e1e9d
szaim <sofiazaim@sofias-MacBook-Pro.local>
2016-07-21 15:33:37 -0400

    sofy's commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
-var question = ["who am I?", ]
+/**var question = ["who am I?", ]
 
 var quiz = {
 	question: 	
 }
+
+
+
 
 var question1 = {
 	prompt: "Who is considered the father of modern symphony?",
@@ -18,5 +21,51 @@ var question1 = {
 
 
 }
+**/
+
+/* constructors */
+/*
+var Question = function(question_text, answers, correctAnswer){
+	this.answers = answers;
+	this.question_text  = question_text;
+	this.correctAnswer = this.answers[correctAnswer];
+}
+
+Question.prototype.checkAnswer = function(value){
 
 
+}
+
+var quiz = [
+	new Question("whois the ?", ["alkjs"], 0),
+]
+
+*/
+
+// working with Object literals
+
+var quiz = [{
+	question: "Who is considered the father of modern symphony?",
+	answer: ["Joseph Hydn", "Wolfgang Mozart", "Franz Schubet", "Ludwig Beethoven"],	
+	correctAnswer: 0
+	},
+	{
+	question: "What type of music existed during the medieval period?",
+	answer: ["chance music", "oops I did it again", "gregorian chant", "Bel Canto Operas"],
+	correctAnswer: 2
+	},
+	{
+	question: "When did expressionism in music flourished?",
+	answer: ["1920 - 1945", "1800 - 1850", "1912 - 1925", "2000 - present"],
+	correctAnswer: 2
+	}    
+];
+function quizAnswer(){
+
+	for(var i = 0; i < quiz.length; i++){
+
+			$(".quiz").text(quiz[i].question);
+		
+	}
+}
+quizAnswer();
