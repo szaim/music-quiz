1c2a5c9f576ea84cf8b27faae3d5887e92467c93
0e0 <ejersonb@gmail.com>
2016-07-21 11:28:28 -0500

    First Push

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var question = ["who am I?", ]
+
+var quiz = {
+	question: 	
+}
+
+var question1 = {
+	prompt: "Who is considered the father of modern symphony?",
+	answer: ["Joseph Hydn", "Wolfgang Mozart", "Franz Schubet", "Ludwig Beethoven"],
+	correctAnswer: function() {
+		if (this.answer === "Joseph Hydn") {
+			console.log("You are correct!");
+		}
+		else {
+			console.log("You are dead wrong!");
+		}
+	}
+
+
+}
+
+
